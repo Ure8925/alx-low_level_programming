5f6cf6d4b13cbc396f53997a99211ef717bf19b1
Ure8925 <suzzylicious24@gmail.com>
2023-02-23 17:13:57 -0800

    Task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 #include <stdio.h>
+
 
 void positive_or_negative(int i); 
 
@@ -11,4 +13,6 @@ void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
 
-#endif /* MAIN_H */
+
+
+#endif
